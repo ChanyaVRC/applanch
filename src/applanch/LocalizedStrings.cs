395ffe293a43afaff7ef648374355bcaf31452b7
ChanyaVRC <63221603+ChanyaVRC@users.noreply.github.com>
2026-04-02 09:32:51 +0900

    Replace reflection with ResourceManager in LocalizedStrings

--- src/applanch/LocalizedStrings.cs
+++ src/applanch/LocalizedStrings.cs
@@ -1,16 +1,21 @@
 using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
 
 namespace applanch;
 
 internal sealed class LocalizedStrings : INotifyPropertyChanged
 {
+    private static readonly ResourceManager ResourceManager =
+        new(typeof(AppResources).FullName!, typeof(AppResources).Assembly);
+
     public static LocalizedStrings Instance { get; } = new();
 
     public string this[string key]
     {
         get
         {
-            var value = typeof(AppResources).GetProperty(key)?.GetValue(null) as string;
+            var value = ResourceManager.GetString(key, CultureInfo.CurrentUICulture);
             return string.IsNullOrEmpty(value) ? key : value;
         }
     }
