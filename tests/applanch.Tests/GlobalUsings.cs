38ebfe82d19654d35d1a9502cba0f131cf27123c
ChanyaVRC <63221603+ChanyaVRC@users.noreply.github.com>
2026-04-17 05:03:31 +0900

    Reorganize directories to match namespaces

--- tests/applanch.Tests/GlobalUsings.cs
+++ tests/applanch.Tests/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using AppResources = applanch.Properties.Resources;
+global using applanch.Core.Utilities;
+global using applanch.Core.Localization;
+global using applanch.Infrastructure.Wpf;
+global using applanch.Infrastructure.Utilities;
